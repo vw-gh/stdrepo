cc64f40f4840c422a4bcd0a205112ff3665cbd7b
vw-gh <vw.kiev@gmail.com>
2015-03-11 00:38:09 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+/**
+ * Created by v.vinogradov on 09.03.2015.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String name = "VTL";
+        int myAge = 33;
+        double weight = 100;
+        weight =100.7;
+        String OurString = name + ": age" + myAge + ", weight:" + weight;
+        System.out.println(OurString);
+        System.out.println(OurString);
+
+    }
+}
